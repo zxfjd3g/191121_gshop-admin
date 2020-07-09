3fc09b472aca7f1b23a07c74be124bdf1387aa1a
zxfjd3g <258147149@qq.com>
2020-07-09 15:03:38 +0800

    SPU

---
+++ src/views/product/components/SpuForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <el-form label-width="120px">
+    <el-form-item label="SPU名称">
+      <el-input type="text" placeholder="SPU名称"></el-input>
+    </el-form-item>
+    <el-form-item label="品牌">
+      <el-select value="">
+        <el-option label="A" value="1"></el-option>
+        <el-option label="B" value="2"></el-option>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="SPU描述">
+      <el-input type="textarea" placeholder="SPU描述" rows="4"></el-input>
+    </el-form-item>
+
+    <el-form-item label="SPU图片">
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+    </el-form-item>
+
+    <el-form-item label="销售属性">
+      <el-select value="">
+        <el-option label="A" value="1"></el-option>
+        <el-option label="B" value="2"></el-option>
+      </el-select>
+      <el-button type="primary" icon="el-icon-plus">添加销售属性</el-button>
+
+      <el-table border style="margin: 20px 0">
+        <el-table-column label="序号" type="index" width="80" align="center"></el-table-column>
+        <el-table-column label="SPU名称" prop="spuName"></el-table-column>
+        <el-table-column label="SPU描述" prop="description"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="{row, $index}">
+            <hint-button title="删除SPU" type="danger" size="mini" icon="el-icon-delete"></hint-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary">保存</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'SpuForm',
+
+  data() {
+    return {
+      dialogImageUrl: '',
+      dialogVisible: false
+    };
+  },
+
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/product/spu/list.vue
+++ src/views/product/spu/list.vue
@@ -1,9 +1,115 @@
 <template>
-  <div>SpuList</div>
+  <div>
+    <el-card style="margin-bottom: 20px">
+      <CategorySelector @categoryChange="handleCategoryChange"/>
+    </el-card>
+    <el-card>
+      <div v-show="!isShowSpuForm">
+        <el-button type="primary" icon="el-icon-plus">添加SPU</el-button>
+      
+        <el-table border :data="spuList" style="margin: 20px 0">
+          <el-table-column label="序号" type="index" width="80" align="center"></el-table-column>
+          <el-table-column label="SPU名称" prop="spuName"></el-table-column>
+          <el-table-column label="SPU描述" prop="description"></el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="{row, $index}">
+              <hint-button title="添加SKU" type="primary" size="mini" icon="el-icon-plus"></hint-button>
+              <hint-button title="修改SPU" type="primary" size="mini" icon="el-icon-edit"></hint-button>
+              <hint-button title="查看SKU" type="info" size="mini" icon="el-icon-info"></hint-button>
+              <hint-button title="删除SPU" type="danger" size="mini" icon="el-icon-delete"></hint-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <el-pagination
+          background
+          style="textAlign:center"
+          :current-page="page"
+          :page-sizes="[3, 6, 9, 12]"
+          :page-size="limit"
+          layout="prev, pager, next, jumper, ->, sizes, total"
+          :total="total"
+          @current-change="getSpuList"
+          @size-change="handleSizeChage">
+        </el-pagination>
+      </div>
+
+      <SpuForm v-show="isShowSpuForm"/>
+
+    </el-card>
+  </div>
 </template>
 
 <script>
+import SpuForm from '../components/SpuForm'
 export default {
   name: 'SpuList',
+
+  data () {
+    return {
+      spuList: [],
+      page: 1,
+      limit: 3,
+      total: 0,
+      category1Id: null,
+      category2Id: null,
+      category3Id: null,
+      isShowSpuForm: true
+    }
+  },
+
+  mounted () {
+    this.getSpuList()
+  },
+
+  methods: {
+
+    /* 
+    选择某个分类的监听回调
+    */
+    handleCategoryChange ({categoryId, level}) {
+      if (level===1) {
+        // 重置二三级数据
+        this.category2Id = null
+        this.category3Id = null
+        this.spuList = []
+
+        this.category1Id = categoryId
+      } else if (level===2) {
+        // 重置三级数据
+        this.category3Id = null
+        this.spuList = []
+
+        this.category2Id = categoryId
+      } else {
+        this.category3Id = categoryId
+        // 当选择了某个三级分类, 请求获取第1页SPU列表
+        this.getSpuList()
+      }
+    },
+
+    /* 
+    获取指定页码的分页数据
+    */
+    async getSpuList (page = 1) {
+      // 保存指定的page
+      this.page = page
+
+      const {limit, category3Id} = this
+      const result = await this.$API.spu.getList(page, limit, category3Id)
+      const {records, total} = result.data
+      this.spuList = records
+      this.total = total
+    },
+
+    handleSizeChage (pageSize) {
+      this.limit = pageSize
+      this.getSpuList(1)
+    }
+  },
+
+  components: {
+    SpuForm
+  }
 }
 </script>
